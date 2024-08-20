eaeff68b5a0d7d2ebc1feb561e76f2406a409d6f
strongchaehwan <122259769+strongchaehwan@users.noreply.github.com>
2024-08-20 15:12:45 +0900

    Title: 오름 내림차순 정렬, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240820/오름 내림차순 정렬/inc-dec-sorting.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i] = scanner.nextInt();
+        }
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        for(int i=0;i< arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]<arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        System.out.println();
+
+        for(int i=0;i< arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+
+
+    }
+
+}
