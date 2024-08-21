52e30cfbb5873241d83705439e074093e057571f
strongchaehwan <122259769+strongchaehwan@users.noreply.github.com>
2024-08-21 09:45:24 +0900

    Title: 두 개의 동일한 수열, Time: 175ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240821/두 개의 동일한 수열/two-equal-series.java
+++ 240821/두 개의 동일한 수열/two-equal-series.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -11,7 +8,6 @@ public class Main {
         int n = sc.nextInt();
         int[] arr1 = new int[n];
         int[] arr2 = new int[n];
-        int count = 0;
 
         for (int i=0;i<n;i++){
             arr1[i] = sc.nextInt();
@@ -21,27 +17,35 @@ public class Main {
             arr2[i] = sc.nextInt();
         }
 
+        sort(arr1);
+        sort(arr2);
+
+        if (equal(n,arr1,arr2)){
+            System.out.println("Yes");
+        }else System.out.println("No");
 
 
-        for(int i=0;i<arr1.length;i++){
-            for (int j=0;j<arr1.length;j++){
-                if (arr1[i]==arr2[j]){
-                    //System.out.println("yes");
-                    count++;
-                }
+    }
+
+    private static void sort(int[] arr) {
+        for (int i=0;i<arr.length;i++){
+           for (int j=0;j<arr.length-i-1;j++){
+               if(arr[j]>arr[j+1]){
+                   int temp = arr[j];
+                   arr[j] = arr[j+1];
+                   arr[j+1] = temp;
+               }
+           }
+       }
+    }
+
+    private static boolean equal(int n, int[] arr1, int[] arr2){
+        for (int i=0;i<n;i++){
+            if(arr1[i] == arr2[i]){
+                return true;
             }
         }
-
-        if (count==n){
-            System.out.println("Yes");
-        }else {
-            System.out.println("No");
-        }
-
-
-
-
-
+        return false;
 
     }
 
