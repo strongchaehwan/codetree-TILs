15619ae1441802a199994283cd7f75979ca7c388
strongchaehwan <122259769+strongchaehwan@users.noreply.github.com>
2024-09-02 22:35:43 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240902/시험 통과 여부 확인하기 2/verify-test-passed-2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] score = new int[4];
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j = 0;j<4;j++){
+                score[j] = scanner.nextInt();
+            }
+
+            int sum = 0;
+            for(int j = 0;j<4;j++){
+                sum += score[j];
+            }
+
+            double avg = (double) sum /4;
+            if (avg > 60){
+                System.out.println("pass");
+                count++;
+            }else{
+                System.out.println("fail");
+            }
+        }
+
+        System.out.println(count);
+
+
+
+
+
+
+    }
+}
