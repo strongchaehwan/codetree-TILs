b2ec1c8efa75ad75c516070b8bb8cd48ea6ef667
strongchaehwan <122259769+strongchaehwan@users.noreply.github.com>
2024-09-03 23:29:54 +0900

    Title: 특정 위치의 문자 2, Time: 110ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240903/특정 위치의 문자 2/char-in-specific-location-2.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char[] arr = new char[10];
+        for(int i=0;i<10;i++){
+            arr[i] = scanner.next().charAt(0);
+        }
+
+        System.out.println(arr[1]+" "+arr[4]+" "+arr[7]);
+
+
+
+
+  }
+}
