9d50c3b8c5d1a5b7c984cea0968d49da9d9a17b6
strongchaehwan <122259769+strongchaehwan@users.noreply.github.com>
2024-10-03 17:12:45 +0900

    Title: 특정 문자의 유무, Time: 115ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241003/특정 문자의 유무/specific-character-presence.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+
+        if (str.contains("ee")) {
+            System.out.print("Yes ");
+        } else
+            System.out.print("No ");
+
+        if (str.contains("ab")) {
+            System.out.print("Yes");
+        } else
+            System.out.print("No");
+
+    }
+
+}
