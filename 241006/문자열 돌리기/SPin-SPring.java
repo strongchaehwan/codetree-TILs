0009c03234ee7435991ec79dfa7ec048b0854488
strongchaehwan <122259769+strongchaehwan@users.noreply.github.com>
2024-10-06 22:53:26 +0900

    Title: 문자열 돌리기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 241006/문자열 돌리기/SPin-SPring.java
+++ 241006/문자열 돌리기/SPin-SPring.java
@@ -6,7 +6,7 @@ public class Main {
         String str = scanner.next();
         System.out.println(str);
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < str.length(); i++) {
 
             str = str.substring(str.length() - 1, str.length()) + str.substring(0, str.length() - 1);
             System.out.println(str);
