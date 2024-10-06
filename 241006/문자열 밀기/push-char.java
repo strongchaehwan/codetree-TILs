8b0f5871fb0eaed48efe0cc14bb60effcd1b74c9
strongchaehwan <122259769+strongchaehwan@users.noreply.github.com>
2024-10-06 22:41:22 +0900

    Title: 문자열 밀기, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 241006/문자열 밀기/push-char.java
+++ 241006/문자열 밀기/push-char.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.next();
 
-        str = str.substring(1, str.length() - 1) + str.substring(0, 1);
+        str = str.substring(1, str.length()) + str.substring(0, 1);
         System.out.println(str);
 
     }
