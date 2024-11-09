fa3e59a2d8a02e86d8a405b8113c88099f0d6daf
strongchaehwan <122259769+strongchaehwan@users.noreply.github.com>
2024-11-10 03:29:08 +0900

    Title: 블럭쌓는 명령2, Time: 134ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 241110/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();//배열의크기
+        int K = scanner.nextInt(); //몇번 반복하는지
+        int[] arr = new int[N];
+
+        for (int k = 0; k < K; k++) {
+            arr[k] = 0;
+        }
+
+        for (int i = 0; i < K; i++) {
+            int start = scanner.nextInt();//첫번쨰로 블럭을 쌓을 배열의 인덱스
+            int end = scanner.nextInt(); //마지막으로 블럭을 쌓을 배열의 인덱스
+
+            for (int j = start; j <= end; j++) {
+                arr[j] += 1;
+            }
+        }
+
+        int max = 0;
+
+        for (int q = 0; q < N; q++) {
+            if (max < arr[q]) {
+                max = arr[q];
+            }
+        }
+
+        System.out.println(max);
+
+
+    }
+
+
+}
