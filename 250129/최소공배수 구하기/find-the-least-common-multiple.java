1c5f5d1bf303c0262a0dc70ddf58ac3d8938d844
strongchaehwan <122259769+strongchaehwan@users.noreply.github.com>
2025-01-29 21:08:12 +0900

    Title: 최소공배수 구하기, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250129/최소공배수 구하기/find-the-least-common-multiple.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        greatest(a, b);
+    }
+
+    private static void greatest(int a, int b) {
+        //gcd = 최대공약수
+        //최소 공배수는 a * b / gcd 한값이 최소공배수
+        int gcd = 0;
+
+        for (int i = 1; i < Math.min(a, b); i++) {
+            if (a % i == 0 && b % i == 0) {
+                gcd = i;
+            }
+
+        }
+        System.out.println(a * b / gcd);
+    }
+}
