724646cbdea4454e832a58ff9858bac697bc848e
strongchaehwan <122259769+strongchaehwan@users.noreply.github.com>
2025-01-29 21:09:20 +0900

    Title: 최소공배수 구하기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 250129/최소공배수 구하기/find-the-least-common-multiple.java
+++ 250129/최소공배수 구하기/find-the-least-common-multiple.java
@@ -13,7 +13,7 @@ public class Main {
         //최소 공배수는 a * b / gcd 한값이 최소공배수
         int gcd = 0;
 
-        for (int i = 1; i < Math.min(a, b); i++) {
+        for (int i = 1; i <= Math.min(a, b); i++) {
             if (a % i == 0 && b % i == 0) {
                 gcd = i;
             }
